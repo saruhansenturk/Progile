1bd1992e312283f86ea3c8ac02bb4179da7ad3d4
SaruGit75 <srhnfrkn75@gmail.com>
2023-12-02 15:57:45 +0300

    feat: create project handler done.

---
+++ Core/Progile.Application/Dtos/Project/CreateProjectDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progile.Application.Dtos.Project
+{
+    public class CreateProjectDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Guid TeamId { get; set; }
+    }
+}
