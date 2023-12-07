cc1dfe48756f71112256908617ca95b041727338
Batug Kocak <batugkocak@gmail.com>
2023-12-07 15:41:26 +0300

    feat: Added Create and SoftDelete for Role Entity
    "Modifier" in BaseEntity could be nullable

---
+++ Core/Progile.Application/Features/Commands/RoleCommands/CreateRoleCommand/RoleCreateCommandHandler.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Progile.Application.Abstraction.Services;
+using Progile.Application.Dtos.Role;
+using Progile.Application.Extensions;
+using Progile.Application.Repositories;
+using Progile.Application.Response;
+using Progile.Domain.Entities;
+
+namespace Progile.Application.Features.Commands.RoleCommands.CreateRoleCommand
+{
+    public class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommandRequest, CommonResponse<CreateRoleDto>>
+    {
+        private readonly IRoleWriteRepository _roleWriteRepository;
+
+
+        public CreateRoleCommandHandler(IRoleWriteRepository roleWriteRepository)
+        {
+            _roleWriteRepository = roleWriteRepository;
+        }
+
+        public async Task<CommonResponse<CreateRoleDto>> Handle(CreateRoleCommandRequest request, CancellationToken cancellationToken)
+        {
+            var createdRole = await _roleWriteRepository.AddAsync(new Role
+            {
+                Name = request.Name,
+                TeamId = request.TeamId,
+                IsActive = true,
+                IsDeleted = false,
+                Creator = "Batug",
+                Modifier = "Batug",
+            });
+
+            var saveChanges = await _roleWriteRepository.SaveAsync();
+
+            if (saveChanges.Data == 1)
+            {
+                return new CommonResponse<CreateRoleDto>
+                {
+                    Data = createdRole?.Data?.MapTo<CreateRoleDto>(),
+                    ResponseStatus = ResponseStatus.Success
+                };
+            }
+
+            return new CommonResponse<CreateRoleDto>
+            {
+                Data = null,
+                ResponseStatus = ResponseStatus.Fail,
+                Message = "An error occurred when inserting data!"
+            };
+        }
+    }
+
+    public class CreateRoleCommandRequest : IRequest<CommonResponse<CreateRoleDto>>
+    {
+        public string Name { get; set; }
+        public Guid TeamId { get; set; }
+
+    }
+}
