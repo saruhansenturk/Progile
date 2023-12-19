e36b59ed2734b6d9065dba539a60b14981af12da
SaruGit75 <srhnfrkn75@gmail.com>
2023-12-19 19:28:04 +0300

    feat: added login service and controller.

--- Api/Progile.API/Controllers/UsersController.cs
+++ Api/Progile.API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Progile.Application.Dtos.Role;
+using Progile.Application.Dtos.Token;
 using Progile.Application.Features.Commands.UserCommands.CreateUserCommands;
+using Progile.Application.Features.Commands.UserCommands.LoginUserCommands;
 using Progile.Application.Response;
 
 namespace Progile.API.Controllers
@@ -17,10 +19,17 @@ namespace Progile.API.Controllers
             _mediator = mediator;
         }
 
-        [HttpPost]
-        public async Task<CommonResponse<bool>> CreateProject(CreateUserCommandRequest request)
+        [HttpPost("[action]")]
+        public async Task<CommonResponse<bool>> CreateUser(CreateUserCommandRequest request)
         {
             CommonResponse<bool> response = await _mediator.Send(request);
+            return response;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<CommonResponse<LoginToken>> LoginUser(LoginUserCommandRequest request)
+        {
+            CommonResponse<LoginToken> response = await _mediator.Send(request);
             return response;
         }
 
